7e33462a933358dec0862cd3c87fb21c63e409ae
sheikhumaid <Sheikhumaid03@gmail.com>
2022-12-17 12:07:12 +0530

    Almost Done

--- setup.py
+++ setup.py
@@ -2,7 +2,27 @@ from setuptools import setup, find_packages
 
 VERSION = '0.0.2'
 DESCRIPTION = 'a package that generates profile picture from your name'
-LONG_DESCRIPTION = 'A package to generate profile picture from a given string and gives you the option to choose the background color of the image. It is a simple package that can be used in any python project. Eg: django, flask, etc. '
+LONG_DESCRIPTION = '''A package to generate profile picture from a given string and gives you the option to choose the background color of the image. It is a simple package that can be used in any python project. Eg: django, flask, etc.
+
+## Installation
+
+
+```bash
+pip install demoProfile
+```
+
+## Usage
+
+```python
+from demoProfile import generate
+img = generate("sheikh umaid")
+img.save("profile.png")
+
+
+```
+
+
+ '''
 
 # Setting up
 setup(
